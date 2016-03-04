def359a75a784e7a1cda35db734ac1a6b6a63edb
Trung Vu <trung.vu@enish.com>
2016-03-04 19:05:43 +0900

    Init EditableImageView class

--- EditableImageView/app/src/main/java/com/trungvu/editableimageview/component/EditableImageView.java
+++ EditableImageView/app/src/main/java/com/trungvu/editableimageview/component/EditableImageView.java
@@ -1,7 +1,11 @@
 package com.trungvu.editableimageview.component;
 
 import android.content.Context;
+import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
 import android.widget.ImageView;
 
 /**
@@ -15,6 +19,13 @@ import android.widget.ImageView;
  */
 public class EditableImageView extends ImageView {
 
+    private static final int INVALID_POINTER_ID = -1;
+
+    protected int mActivePointerId = INVALID_POINTER_ID;
+    protected ScaleGestureDetector mScaleGestureDetector;
+
+    private ScaleListener mScaleListener;
+
     public EditableImageView(Context context) {
         this(context, null);
     }
@@ -25,5 +36,28 @@ public class EditableImageView extends ImageView {
 
     public EditableImageView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        mScaleListener = new ScaleListener();
+        mScaleGestureDetector = new ScaleGestureDetector(context, mScaleListener);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mScaleGestureDetector.onTouchEvent(event);
+        final int action = MotionEventCompat.getActionMasked(event);
+        switch (action) {
+            case MotionEvent.ACTION_POINTER_DOWN: {
+                
+                break;
+            }
+        }
+        return true;
+    }
+
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            return super.onScale(detector);
+        }
     }
 }
